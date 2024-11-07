398124ef44d529bf70b4cb42b60bcf7419b4cc52
bentiriaya <bentiriaya2003@gmail.com>
2024-11-07 21:19:03 +0100

    edit version

--- lib/views/myRecipes.dart
+++ lib/views/myRecipes.dart
@@ -1,33 +1,23 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data/colors.dart';
-import 'package:flutter_application_1/data/strings.dart';
 import 'package:flutter_application_1/routes/routesnames.dart';
 import 'package:get/get.dart';
 import 'package:flutter_application_1/Controllers/myRecipesController.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../Services/sharedperfsManagers.dart';
+import '../data/strings.dart';
 import '../model/recipe.dart';
 
 class MyRecipes extends StatelessWidget {
   MyRecipes({super.key});
-  final SharedperfManager sp=Get.put(SharedperfManager());
+  final SharedperfManager sp = Get.put(SharedperfManager());
   final MyRecipesController controller = Get.put(MyRecipesController());
-
   Future<Icon> _getIconForType(String type) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? iconString = prefs.getString('selectedIcon');
-
-    // Si on n'a pas d'icône enregistrée, retourner une icône par défaut
-    if (iconString == null) {
-      return Icon(Icons.fastfood, size: 50);
-    }
-
-
-
-    return Data.recipeTypes2[type] ?? Icon(Icons.fastfood, size: 50); // Icône par défaut si type inconnu
+    // Retourne l'icône par défaut si aucune icône n'est enregistrée
+    return Data.recipeTypes2[type] ?? Icon(Icons.fastfood, size: 50);
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +27,6 @@ class MyRecipes extends StatelessWidget {
           itemCount: controller.recipes.length,
           itemBuilder: (context, index) {
             final recipe = controller.recipes[index];
-
             return FutureBuilder<Icon>(
               future: _getIconForType(recipe.type), // Récupérer l'icône associée au type
               builder: (context, snapshot) {
@@ -52,38 +41,66 @@ class MyRecipes extends StatelessWidget {
                 // Utiliser l'icône récupérée
                 Icon icon = snapshot.data ?? Icon(Icons.fastfood, size: 50);
 
-                return ListTile(
-                  leading: recipe.imageUrl != null && recipe.imageUrl!.isNotEmpty
-                      ? ClipOval(
-                    child: Image.asset(
-                      recipe.imageUrl!,
-                      width: 50,
-                      height: 50,
-                      fit: BoxFit.cover,
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 2,
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(10.0),
+                    leading: recipe.imageUrl != null && recipe.imageUrl!.isNotEmpty
+                        ? ClipOval(
+                      child: Image.asset(
+                        recipe.imageUrl!,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                        : icon, // Utiliser l'icône obtenue si aucune image
+                    title: Text(
+                      recipe.title,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                  )
-                      : icon, // Utiliser l'icône obtenue
-                  title: Text(recipe.title),
-                  subtitle: Text(recipe.cooktime),
+                    subtitle: Text(
+                      recipe.cooktime,
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.edit, color: color.yellow),
+                      onPressed: () {
+                        // Action de modification (par exemple, navigation vers la page d'édition)
+                       // controller.editRecipe(recipe);
+                      },
+                    ),
+                    onTap: () async {
+                      final rec = RecipeModel(
+                        id: recipe.id,
+                        title: recipe.title,
+                        ingredients: recipe.ingredients,
+                        instructions: recipe.instructions,
+                        imageUrl: recipe.imageUrl,
+                        type: recipe.type,
+                        cooktime: recipe.cooktime,
+                      ).toMap();
+                      controller.gotoDetails(rec);
+                      //  await sp.saveInt("id", recipe["id"]);
+                      print('${recipe.id}recipe.id');
+                      await sp.saveInt("id", recipe.id);
+                      await sp.saveString("title",recipe.title);
+                      await sp.saveString("instructions", recipe.instructions);
+                      await sp.saveStringList("ingredients", recipe.ingredients);
+                      await sp.saveString("imageUrl", recipe.imageUrl);
+                      await sp.saveString("cooktime", recipe.cooktime);
+                      // Passez les détails de la recette vers la page RecipeDetailsPage
+                      Get.toNamed(
+                        namesRoute.detailRec,
 
-                  onTap: () async{
-                   final rec=RecipeModel(id:recipe.id,title:recipe.title,ingredients:recipe.ingredients,instructions:recipe.instructions,imageUrl:recipe.imageUrl,type:recipe.type,cooktime:recipe.cooktime).toMap();
-                    controller.gotoDetails(rec);
-                 //  await sp.saveInt("id", recipe["id"]);
-                    print('${recipe.id}recipe.id');
-                    await sp.saveInt("id", recipe.id);
-                    await sp.saveString("title",recipe.title);
-                    await sp.saveString("instructions", recipe.instructions);
-                    await sp.saveStringList("ingredients", recipe.ingredients);
-                    await sp.saveString("imageUrl", recipe.imageUrl);
-                    await sp.saveString("cooktime", recipe.cooktime);
-                    // Passez les détails de la recette vers la page RecipeDetailsPage
-                    Get.toNamed(
-                      namesRoute.detailRec,
 
-
-                    );
-                  },
+                      );
+                    },
+                  ),
                 );
               },
             );
