9142b75272c1e5394f250078d103554089d48b3f
bentiriaya <bentiriaya2003@gmail.com>
2024-11-08 17:24:58 +0100

    ourecipes v2

--- lib/Controllers/RecipeDetailController.dart
+++ lib/Controllers/RecipeDetailController.dart
@@ -1,41 +1,27 @@
 import 'package:get/get.dart';
 import '../Services/sharedperfsManagers.dart';
-import '../data/apisource.dart';
 
 class RecipeDetailController extends GetxController {
+  // Déclaration des variables observables
   var id = 0.obs;
   var title = ''.obs;
   var cookTime = ''.obs;
   var thumbnailUrl = ''.obs;
   var instructions = ''.obs;
   var ingredients = <String>[].obs;
-  var isFavorite = false.obs;
+
 
   @override
   void onInit() async {
     super.onInit();
     final SharedperfManager sp = Get.find<SharedperfManager>();
 
+    // Attendre que toutes les données soient chargées avant d'assigner les valeurs
     id.value = await sp.getInt("id") ?? 0;
     title.value = await sp.getString("title") ?? "No Title";
     cookTime.value = await sp.getString("cooktime") ?? "N/A";
     thumbnailUrl.value = await sp.getString("imageUrl") ?? "";
     instructions.value = await sp.getString("instructions") ?? "No instructions";
     ingredients.value = await sp.getStringList("ingredients") ?? ["No ingredients"];
-    isFavorite.value = await sp.getBool("isFavorite") ?? false; // Nouvelle ligne
-  }
-
-  void toggleFavorite() async {
-    isFavorite.value = !isFavorite.value;
-
-    // Mettez à jour la valeur `isFavorite` dans SharedPreferences ou tout autre stockage
-    final SharedperfManager sp = Get.find<SharedperfManager>();
-    await sp.saveBool("isFavorite", isFavorite.value);
-
-    // Optionnel : mettez à jour la valeur dans votre liste `recipes`
-    var recipe = recipes.firstWhere((r) => r['id'] == id.value);
-    if (recipe != null) {
-      recipe['isFavorite'] = isFavorite.value;
-    }
   }
 }
